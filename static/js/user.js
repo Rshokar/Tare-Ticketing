784af57497d5f56ac63feffce42be1a8c5f95c73
Nosewall <nolannordwall@gmail.com>
2021-10-09 14:41:45 -0700

    Login page cleaned

---
+++ static/js/user.js
@@ -0,0 +1,10 @@
+let user = class {
+  constructor(email, firstName, lastName, company, accountType) {
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.company = company;
+    this.accountType = accountType;
+
+  }
+}
